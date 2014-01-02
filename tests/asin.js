2d88288534a92956aaebe1249f2a04fa49f8d65e
Juho Vepsalainen <bebraw@gmail.com>
2014-01-02 11:39:47 +0200

    Fix test

--- tests/asin.js
+++ tests/asin.js
@@ -2,6 +2,6 @@ var between = require('../').between;
 
 
 module.exports = function(op, a) {
-    return f.between(op(a), -Math.PI / 2, Math.PI / 2);
+    return between(op(a), -Math.PI / 2, Math.PI / 2);
 };
 
